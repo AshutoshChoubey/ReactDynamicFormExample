6507930a73910718e32aca70a3e0f464e054446a
Ashutosh <ashuashutoshchoubey@gmail.com>
2020-09-22 16:34:40 +0530

    done

--- src/components/LooseCarpentry/Carpentry.js
+++ src/components/LooseCarpentry/Carpentry.js
@@ -12,10 +12,9 @@ class Carpentry extends Component {
             { index: Math.random(), materialDisplayName: "Display Material Name2", materialName: false, amount: "", brandSpeci: "", date: "", time: "" },
             { index: Math.random(), materialDisplayName: "Display Material Name3", materialName: false, amount: "", brandSpeci: "", date: "", time: "" },
             { index: Math.random(), materialDisplayName: "Display Material Name4", materialName: false, amount: "", brandSpeci: "", date: "", time: "" },
-            { index: Math.random(), materialDisplayName: "Display Material Name5", materialName: false, amount: "", brandSpeci: "", date: "", time: "" }, 
+            { index: Math.random(), materialDisplayName: "Display Material Name5", materialName: false, amount: "", brandSpeci: "", date: "", time: "" },
             ],
-            labourList: [{ index: Math.random(), materialDisplayName: "Display  Name1", area: false, startDate: "", endDate: "", startTime: "", endTime: "",descripition:"",skilled:"",semiskilled:"",nonskilled:"",totalWork:"",uploaddrawing:"" },
-            { index: Math.random(), materialDisplayName: "Display  Name2", area: false, startDate: "", endDate: "", startTime: "", endTime: "",descripition:"",skilled:"",semiskilled:"",nonskilled:"",totalWork:"",uploaddrawing:"" },{ index: Math.random(), materialDisplayName: "Display  Name3", area: false, startDate: "", endDate: "", startTime: "", endTime: "",descripition:"",skilled:"",semiskilled:"",nonskilled:"",totalWork:"",uploaddrawing:"" },
+            labourList: [{ index: Math.random(), materialDisplayName: "Display  Name1", area: false, startDate: "", endDate: "", startTime: "", endTime: "", descripition: "", skilled: "", semiskilled: "", nonskilled: "", totalWork: "", uploaddrawing: "" },
             ],
             wardrobe: true,
 
@@ -23,23 +22,23 @@ class Carpentry extends Component {
     }
     handleChange = (e) => {
 
-        if (["amount","brandSpeci","date","time"].includes(e.target.name)) {
+        if (["amount", "brandSpeci", "date", "time"].includes(e.target.name)) {
             let matrialList = [...this.state.matrialList]
             matrialList[e.target.dataset.id][e.target.name] = e.target.value;
-        } 
-        else if (["area","startDate","endDate","startTime","endTime","descripition","skilled:","semiskilled:","nonskilled","totalWork","uploaddrawing"].includes(e.target.name)) {
-            let labourList = [...this.state.matrialList]
+        }
+        else if (["area", "startDate", "endDate", "startTime", "endTime", "descripition", "skilled", "semiskilled", "nonskilled", "totalWork", "uploaddrawing"].includes(e.target.name)) {
+            let labourList = [...this.state.labourList]
             labourList[e.target.dataset.id][e.target.name] = e.target.value;
         }
         else if (["materialName"].includes(e.target.name)) {
-            
+
             let matrialList = [...this.state.matrialList]
             console.log(matrialList[e.target.dataset.id][e.target.name])
             if (matrialList[e.target.dataset.id][e.target.name] === true || matrialList[e.target.dataset.id][e.target.name] === 'on') {
-                 matrialList[e.target.dataset.id][e.target.name] = false;
+                matrialList[e.target.dataset.id][e.target.name] = false;
             }
-            if (matrialList[e.target.dataset.id][e.target.name] === false ||matrialList[e.target.dataset.id][e.target.name] === "false") {
-                 matrialList[e.target.dataset.id][e.target.name] = true;
+            if (matrialList[e.target.dataset.id][e.target.name] === false || matrialList[e.target.dataset.id][e.target.name] === "false") {
+                matrialList[e.target.dataset.id][e.target.name] = true;
             }
             //this.setState({ [e.target.name]: e.target.value })
         }
@@ -51,15 +50,19 @@ class Carpentry extends Component {
     checkboxValue = (name, val) => {
 
     }
-   
-   
+    addLabour = (e) => {
+        this.setState((prevState) => ({
+            labourList: [...prevState.labourList, { index: Math.random(), materialDisplayName: "Display  Name1", area: false, startDate: "", endDate: "", startTime: "", endTime: "", descripition: "", skilled: "", semiskilled: "", nonskilled: "", totalWork: "", uploaddrawing: "" }],
+        }));
+    }
+
     handleSubmit = (e) => {
         e.preventDefault();
         console.log(this.state)
     }
-    showTask = (e) => {
-        this.setState({ "showTask": true })
-
+    submitMaterial = (e) => {
+        e.preventDefault();
+        console.log(this.state.matrialList)
     }
     render() {
         let { zoneList } = this.state//let { carpentry, zoneList } = this.state
@@ -81,27 +84,29 @@ class Carpentry extends Component {
                             <form onSubmit={this.handleSubmit} onChange={this.handleChange} >
                                 <div className="row scrollmenu"  >
                                     {
-          this.state.matrialList.map((val, idx) => {
-        return (
-            <div key={val.index} className="col-sm-3" ><MaterialName checkboxValue={this.checkboxValue} idx={idx} passedData={val} /></div>
-        )
-    })
+                                        this.state.matrialList.map((val, idx) => {
+                                            return (
+                                                <div key={val.index} className="col-sm-3" ><MaterialName checkboxValue={this.checkboxValue} idx={idx} passedData={val} /></div>
+                                            )
+                                        })
 
-      }
+                                    }
 
-                                    
+<div className="col-sm-2">
+<button type="button" onClick={this.submitMaterial} className="btn btn-primary text-center">Order</button>
+</div>
                                 </div>
 
                                 {
-          this.state.labourList.map((val, idx) => {
-        return (
-            <div className="row" key={val.index}  ><div  className="col-sm-12" ><Labour  idx={idx} passedData={val} /></div></div>
-        )
-    })
+                                    this.state.labourList.map((val, idx) => {
+                                        return (
+                                            <div  className="row" key={val.index}  ><div className="col-sm-12" ><Labour addLabour={this.addLabour} idx={idx} passedData={val} /></div></div>
+                                        )
+                                    })
 
-      }
+                              }
+                              
 
-                                
                                 <div className="card-footer text-center"> <button type="submit" className="btn btn-primary text-center">Submit</button></div>
                             </form>
                         </div>
--- src/components/LooseCarpentry/Labour.js
+++ src/components/LooseCarpentry/Labour.js
@@ -9,69 +9,89 @@ class Labour extends Component {
     render() {
         return (
             <Fragment >
-            <div className="card">
-                <div className="card-body" >
-                    <div className="form-group form-check form-check-inline">
-                        {this.props.passedData.materialDisplayName}
-                    </div>
-
-                    <div className="row" >
-                        <div className="col-sm-8" > <div className="form-group "><input type="text" placeholder="Area" name="area" data-id={this.props.idx} className="form-control" /></div></div>
-                    </div>
-                    <div className="row" >
-                        <div className="col-sm-6" > <div className="form-group "><input type="date" placeholder="Start Date" name="startDate" data-id={this.props.idx} className="form-control" /></div></div>
-                        <div className="col-sm-6" > <div className="form-group "><input type="date" placeholder="End Date" name="endDate" data-id={this.props.idx} className="form-control" /></div></div>
-                    </div>
-                    <div className="row" >
-                        <div className="col-sm-6" > <div className="form-group "><input type="time" placeholder="Start Time" name="startTime" data-id={this.props.idx} className="form-control" /></div></div>
-                        <div className="col-sm-6" > <div className="form-group "><input type="time" placeholder="End Time" name="endTime" data-id={this.props.idx} className="form-control" /></div></div>
-                    </div>
-                </div>
-            </div>
-            <div className="card">
-            <div className="card-body" >
-                <div className="form-group form-check form-check-inline">
-                   Description
-                </div>
-
                 <div className="row" >
-                    <div className="col-sm-8" > <div className="form-group "><input type="text" placeholder="Description" name="descripition" data-id={this.props.idx} className="form-control" /></div></div>
+                    <div className="col-sm-3" >
+                        <div className="card">
+                            <div className="card-body" >
+                                <div className="form-group form-check form-check-inline">
+                                    {this.props.passedData.materialDisplayName}
+                                </div>
+
+                                <div className="row" >
+                                    <div className="col-sm-8" > <div className="form-group "><input type="text" placeholder="Area" name="area" data-id={this.props.idx} className="form-control" /></div></div>
+                                </div>
+                                <div className="row" >
+                                    <div className="col-sm-6" > <div className="form-group "><input type="date" placeholder="Start Date" name="startDate" data-id={this.props.idx} className="form-control" /></div></div>
+                                    <div className="col-sm-6" > <div className="form-group "><input type="date" placeholder="End Date" name="endDate" data-id={this.props.idx} className="form-control" /></div></div>
+                                </div>
+                                <div className="row" >
+                                    <div className="col-sm-6" > <div className="form-group "><input type="time" placeholder="Start Time" name="startTime" data-id={this.props.idx} className="form-control" /></div></div>
+                                    <div className="col-sm-6" > <div className="form-group "><input type="time" placeholder="End Time" name="endTime" data-id={this.props.idx} className="form-control" /></div></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-sm-3" >
+                        <div className="card">
+                            <div className="card-body" >
+                                <div className="form-group form-check form-check-inline">
+                                    Description
                 </div>
-                
-            </div>
-        </div>
-        <div className="card">
-        <div className="card-body" >
-            <div className="form-group form-check form-check-inline">
-               Laboor Reuired
+
+                                <div className="row" >
+                                    <div className="col-sm-8" > <div className="form-group "><input type="text" placeholder="Description" name="descripition" data-id={this.props.idx} className="form-control" /></div></div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-sm-3" ><div className="card">
+                        <div className="card-body" >
+                            <div className="form-group form-check form-check-inline">
+                                Laboor Reuired
             </div>
 
-            <div className="row" >
-                <div className="col-sm-12" > <div className="form-group "><input type="text" placeholder="Skilled" name="skilled" data-id={this.props.idx} className="form-control" /></div></div>
-            </div>
-            <div className="row" >
-                <div className="col-sm-12" > <div className="form-group "><input type="" placeholder="Semi skilled" name="semiskilled" data-id={this.props.idx} className="form-control" /></div></div>
-            </div>
-            <div className="row" >
-                <div className="col-sm-6" > <div className="form-group "><input type="time" placeholder="Non Silled" name="nonskilled" data-id={this.props.idx} className="form-control" /></div></div>
-            </div>
-        </div>
-    </div>
-    <div className="card">
-        <div className="card-body" >
-            <div className="form-group form-check form-check-inline">
-               Total Work
+                            <div className="row" >
+                                <div className="col-sm-12" > <div className="form-group "><input type="text" placeholder="Skilled" name="skilled" data-id={this.props.idx} className="form-control" /></div></div>
+                            </div>
+                            <div className="row" >
+                                <div className="col-sm-12" > <div className="form-group "><input type="" placeholder="Semi skilled" name="semiskilled" data-id={this.props.idx} className="form-control" /></div></div>
+                            </div>
+                            <div className="row" >
+                                <div className="col-sm-6" > <div className="form-group "><input type="time" placeholder="Non Silled" name="nonskilled" data-id={this.props.idx} className="form-control" /></div></div>
+                            </div>
+                        </div>
+                    </div></div>
+                    <div className="col-sm-3" >
+                        <div className="card">
+                            <div className="card-body" >
+                                <div className="form-group form-check form-check-inline">
+                                    Total Work
             </div>
 
-            <div className="row" >
-                <div className="col-sm-12" > <div className="form-group "><input type="text" placeholder="Total work" name="totalWork" data-id={this.props.idx} className="form-control" /></div></div>
-            </div>
-            <div className="row" >
-                <div className="col-sm-12" > <div className="form-group "><input type="" placeholder="Upload Drawing" name="uploaddrawing" data-id={this.props.idx} className="form-control" /></div></div>
-            </div>
-        </div>
-    </div>
-    </Fragment>
+                                <div className="row" >
+                                    <div className="col-sm-12" > <div className="form-group "><input type="text" placeholder="Total work" name="totalWork" data-id={this.props.idx} className="form-control" /></div></div>
+                                </div>
+                                <div className="row" >
+                                    <div className="col-sm-12" > <div className="form-group "><input type="" placeholder="Upload Drawing" name="uploaddrawing" data-id={this.props.idx} className="form-control" /></div></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="row">  
+<div className="col-sm-2">
+<button type="button" className="btn btn-primary text-center">Order</button>
+</div>
+<div className="col-sm-2">
+<button type="button" onClick={()=>this.props.addLabour()} className="btn btn-primary text-center">+</button>
+</div>
+</div>
+
+
+
+
+            </Fragment>
         )
     }
 
